0a9d82ccd76284bb7fbd2181a8da04c3b05c825b
Anna Kremer <sheikoanna@gmail.com>
2018-09-27 16:14:10 -0700

    Add sign up async

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import SignUpForm from './signup-form';
+import {Link, Redirect} from 'react-router-dom';
 
-export default function SignUp() {
+export function SignUp(props) {
+	// If we are logged in (which happens automatically when registration
+    // is successful) redirect to the user's dashboard
+    if (props.loggedIn) {
+        return <Redirect to="/dashboard" />;
+    }
 	return(
 		<section className="login-screen" aria-live="assertive">
 			<SignUpForm />
@@ -15,3 +22,9 @@ export default function SignUp() {
 			</section> 
 		)
 }
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(SignUp);
--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,24 +1,64 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Field, reduxForm, focus} from 'redux-form';
+import {registerUser} from '../actions/users';
+import {login} from '../actions/auth';
+import Input from './input';
+import {required, nonEmpty, matches, length, isTrimmed} from '../validators';
+const passwordLength = length({min: 5, max: 15});
+const matchesPassword = matches('password');
 
-export default function SignUpForm() {
+export class SignUpForm extends React.Component {
+	onSubmit(values) {
+		        const {username, password} = values;
+		        const user = {username, password};
+		        return this.props
+		        	.dispatch(registerUser(user))
+		        	.then(() => this.props.dispatch(login(username, password)))
+	}
+
+	render() {
+
 	return (
-		<form role="form" className="signup">
+		<form role="form" className="signup"
+		onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
 					<fieldset name="signup-info">
 						<div className="login-header">
 							<legend align="center">Sign Up</legend>
 				   		 </div>
 				    	<p id='notification'></p>
 
-						<label htmlFor="email" required>Email</label>
-						<input type="email" name="email" id="email" placeholder="Email address" required="" />
+						<label htmlFor="username" required>Email</label>
+						<Field
+                    component={Input}
+                    type="email"
+                    name="username"
+                    validate={[required, nonEmpty, isTrimmed]}
+                />
 						<label htmlFor="password" required>Password</label>
-						<input type="password" name="password" id="password" placeholder="Password" required />
+						 <Field
+                    component={Input}
+                    type="password"
+                    name="password"
+                    validate={[required, passwordLength, isTrimmed]}
+                />
 						<label htmlFor="password-confirm" required>Confirm password</label>
-						<input type="password" name="password" id="password-confirm" placeholder="Confirm password" required />
+						 <Field
+                    component={Input}
+                    type="password"
+                    name="passwordConfirm"
+                    validate={[required, nonEmpty, matchesPassword]}
+                />
 				</fieldset>
-					<button type="submit" className="js-login-button">Login</button>
+					<button type="submit" className="js-login-button" disabled={this.props.pristine || this.props.submitting}>Login</button>
 					<p className="login-here">Already have an account? <Link to="/login">Log in</Link></p>
 				</form>
 		)
+	}
 }
+
+export default reduxForm({
+    form: 'registration',
+    onSubmitFail: (errors, dispatch) =>
+        dispatch(focus('registration', Object.keys(errors)[0]))
+})(SignUpForm);
